c7ff98471d59845c4973784d2ce77ddc3162ecaf
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-15 15:15:13 +0400

    Implement getter and setter

--- Department.java
+++ Department.java
@@ -25,4 +25,60 @@ public class Department {
 
     public Department() {
     }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getHeadDoctorId() {
+        return headDoctorId;
+    }
+
+    public void setHeadDoctorId(String headDoctorId) {
+        this.headDoctorId = headDoctorId;
+    }
+
+    public List<Doctor> getDoctors() {
+        return doctors;
+    }
+
+    public void setDoctors(List<Doctor> doctors) {
+        this.doctors = doctors;
+    }
+
+    public List<Nurse> getNurses() {
+        return nurses;
+    }
+
+    public void setNurses(List<Nurse> nurses) {
+        this.nurses = nurses;
+    }
+
+    public Integer getBedCapacity() {
+        return bedCapacity;
+    }
+
+    public void setBedCapacity(Integer bedCapacity) {
+        this.bedCapacity = bedCapacity;
+    }
+
+    public Integer getAvailableBeds() {
+        return availableBeds;
+    }
+
+    public void setAvailableBeds(Integer availableBeds) {
+        this.availableBeds = availableBeds;
+    }
 }
