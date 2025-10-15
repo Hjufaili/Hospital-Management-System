fa085ec053809ad773f131af65c5710200d36ddc
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-15 16:20:43 +0400

    Implement removeDoctor method

--- DoctorService.java
+++ DoctorService.java
@@ -21,13 +21,24 @@ public class DoctorService {
     public static void editDoctor(String doctorId, Doctor updatedDoctor) {
 
         for (int i = 0; i < doctors.size(); i++) {
-            if (doctors.get(i).getId().equals(doctorId)){
+            if (doctors.get(i).getDoctorId().equals(doctorId)){
                 doctors.add(i,updatedDoctor);
                 System.out.println("updated successfully!");
                 return;
             }
         }
         System.out.println("Not found doctor:");
+    }
+
+    public static void removeDoctor(String doctorId) {
+        boolean removed = doctors.removeIf(d ->d.getDoctorId().equals(doctorId));
+
+        if (removed){
+            System.out.println("Removed doctor:  "+ doctorId);
+        }else {
+            System.out.println("Not found doctor");
+        }
+
     }
 
 }
