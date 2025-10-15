25079aab608822005af921645756374828a6e580
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-15 16:05:01 +0400

    Implement addDoctor method

--- DoctorService.java
+++ DoctorService.java
@@ -1,5 +1,22 @@
 package Service;
 
+import Entity.Doctor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DoctorService {
+    private static List<Doctor> doctors = new ArrayList<>();
+
+
+    public static void addDoctor(Doctor doctor) {
+        if (doctor != null) {
+            doctors.add(doctor);
+            System.out.println("Doctor is added");
+        } else {
+            System.out.println("Cannot null doctor");
+        }
+
+    }
 
 }
