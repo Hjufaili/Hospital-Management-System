9a013cb5e239435e46b45287dd3de26018dab244
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-15 15:00:07 +0400

    Create Appointment class

---
+++ Appointment.java
@@ -0,0 +1,16 @@
+package Entity;
+
+import java.time.LocalDate;
+
+public class Appointment {
+    private String appointmentId;
+    private String patientId;
+    private String doctorId;
+    private LocalDate appointmentDate;
+    private String appointmentTime;
+    private String status;
+    private String reason;
+    private String notes;
+
+
+}
