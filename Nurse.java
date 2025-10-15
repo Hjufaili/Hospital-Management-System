dbce45372325edb3794066d2d3f84c694dd03eeb
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-15 14:29:37 +0400

    create Nurse class

---
+++ Nurse.java
@@ -0,0 +1,15 @@
+package Entity;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Stack;
+
+public class Nurse extends Person {
+    private String nurseId;
+    private String departmentIdl;
+    private String shift;
+    private String qualification;
+    private List <String> assignedPatients;
+
+    
+}
