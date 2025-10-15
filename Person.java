1ab51818940398eeddc51fe6d0f4bdd05360e4e6
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-15 14:10:53 +0400

    create Person class

---
+++ Person.java
@@ -0,0 +1,19 @@
+package Entity;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Person {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+
+}
