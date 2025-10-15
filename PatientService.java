435c5038bb78808e8ae9e4377c5390624b1980e4
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-15 15:26:33 +0400

    Implement addPatient method

--- PatientService.java
+++ PatientService.java
@@ -4,5 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PatientService {
-    
+    private static List<Patient> patients = new ArrayList<>();
+
+    public static void addPatient(Patient patient) {
+        if (patient != null) {
+            patients.add(patient);
+            System.out.println("Patient added: ");
+        } else {
+            System.out.println("Cannot null patient.");
+        }
+    }
 }
