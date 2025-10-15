f84357065a33f8fd50c67c4a80d6c2115862ffdd
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-15 14:30:27 +0400

    Implement constructor chaining

--- Nurse.java
+++ Nurse.java
@@ -11,5 +11,24 @@ public class Nurse extends Person {
     private String qualification;
     private List <String> assignedPatients;
 
-    
+    public Nurse(String id, String firstName, String lastName, LocalDate dateOfBirth,
+                 String gender, String phoneNumber, String address, String email,
+                 String nurseId, String departmentIdl, String shift,
+                 String qualification, List<String> assignedPatients) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, address, email);
+        this.nurseId = nurseId;
+        this.departmentIdl = departmentIdl;
+        this.shift = shift;
+        this.qualification = qualification;
+        this.assignedPatients = assignedPatients;
+    }
+
+    public Nurse(String nurseId, String departmentIdl, String shift,
+                 String qualification, List<String> assignedPatients) {
+        this.nurseId = nurseId;
+        this.departmentIdl = departmentIdl;
+        this.shift = shift;
+        this.qualification = qualification;
+        this.assignedPatients = assignedPatients;
+    }
 }
