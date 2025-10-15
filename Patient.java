34e3a409d8ddacd5e430e8fdc9967fe1c9199a55
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-15 14:22:54 +0400

    Implement constructor chaining to parent const.

--- Patient.java
+++ Patient.java
@@ -14,4 +14,34 @@ public class Patient extends Person {
     private List<MedicalRecord> medicalRecords;
     private List<Appointment> appointments;
 
+    public Patient(String id, String firstName, String lastName, LocalDate dateOfBirth,
+                   String gender, String phoneNumber, String address, String email,
+                   String patientId, String bloodGroup, List<String> allergies, String emergencyContact,
+                   LocalDate registrationDate, String insuranceId,
+                   List<MedicalRecord> medicalRecords, List<Appointment> appointments) {
+
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, address, email);
+        this.patientId = patientId;
+        this.bloodGroup = bloodGroup;
+        this.allergies = allergies;
+        this.emergencyContact = emergencyContact;
+        this.registrationDate = registrationDate;
+        this.insuranceId = insuranceId;
+        this.medicalRecords = medicalRecords;
+        this.appointments = appointments;
+    }
+
+    public Patient(String patientId, String bloodGroup, List<String> allergies, String emergencyContact,
+                   LocalDate registrationDate, String insuranceId,
+                   List<MedicalRecord> medicalRecords, List<Appointment> appointments) {
+        this.patientId = patientId;
+        this.bloodGroup = bloodGroup;
+        this.allergies = allergies;
+        this.emergencyContact = emergencyContact;
+        this.registrationDate = registrationDate;
+        this.insuranceId = insuranceId;
+        this.medicalRecords = medicalRecords;
+        this.appointments = appointments;
+    }
+
 }
