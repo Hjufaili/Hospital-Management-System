e8deb467ad58a72cd564a0b3497f3107a7d54dcc
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-16 10:30:40 +0400

    Implement addMedicalRecord method

--- MedicalRecordService.java
+++ MedicalRecordService.java
@@ -7,6 +7,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MedicalRecordService {
+    private static List<MedicalRecord> medicalRecords = new ArrayList<>();
 
-    
+    public static void addMedicalRecord(MedicalRecord medicalRecord) {
+        if (medicalRecord != null) {
+            medicalRecords.add(medicalRecord);
+            System.out.println("Medical record is added: "+medicalRecord);
+        }
+        System.out.println("Cannot add null Medical record ");
+
+    }
 }
