56d26e031de72fb9b2a761dcdc019e970242f7b6
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-16 11:48:49 +0400

    Implement addAppointment method

--- AppointmentService.java
+++ AppointmentService.java
@@ -1,6 +1,20 @@
 package Service;
 
+import Entity.Appointment;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppointmentService {
 
-    
+    private static List<Appointment> appointments = new ArrayList<>();
+
+
+    public static void addAppointment(Appointment appointment) {
+        if (appointment !=null){
+            appointments.add(appointment);
+            System.out.println("appointment is added: " + appointment);
+        }
+        System.out.println("Cannot add null appointment ");
+    }
 }
