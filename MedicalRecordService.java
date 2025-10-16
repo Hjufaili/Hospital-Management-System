0a45b8c2c4574ebd8105e2663bb475856515a4c8
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-16 10:39:01 +0400

    Implement editMedicalRecord method

--- MedicalRecordService.java
+++ MedicalRecordService.java
@@ -12,9 +12,20 @@ public class MedicalRecordService {
     public static void addMedicalRecord(MedicalRecord medicalRecord) {
         if (medicalRecord != null) {
             medicalRecords.add(medicalRecord);
-            System.out.println("Medical record is added: "+medicalRecord);
+            System.out.println("Medical record is added: " + medicalRecord);
         }
         System.out.println("Cannot add null Medical record ");
 
+    }
+
+    public static void editMedicalRecord(String medicalRecordsId, MedicalRecord updatedmedicalRecord) {
+        for (int i = 0; i < medicalRecords.size(); i++) {
+            if (medicalRecords.get(i).getRecordId().equals(medicalRecordsId)) {
+                medicalRecords.set(i, updatedmedicalRecord);
+                System.out.println("MedicalRecord is updated: " + medicalRecordsId);
+                return;
+            }
+        }
+        System.out.println("MedicalRecordId not found: " + medicalRecordsId);
     }
 }
