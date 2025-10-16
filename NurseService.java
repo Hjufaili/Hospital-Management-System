69bb21adf303dda907d11e19e238fe1ec5bbe103
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-16 09:04:19 +0400

    Implement addNurse method

--- NurseService.java
+++ NurseService.java
@@ -1,7 +1,21 @@
 package Service;
 
 
+import Entity.Nurse;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NurseService {
+    private static List<Nurse> nurses = new ArrayList<>();
 
 
+    public static void addNurse(Nurse nurse) {
+        if (nurse !=null){
+            nurses.add(nurse);
+            System.out.println("Nurse added");
+        }else {
+            System.out.println("Cannot add null nurse");
+        }
+    }
 }
