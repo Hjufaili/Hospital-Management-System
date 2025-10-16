d19fa77dd74ce788a68f388a23715dc9f2ce9e10
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-16 14:54:22 +0400

    Implement addDepartment method

--- DepartmentService.java
+++ DepartmentService.java
@@ -1,6 +1,19 @@
 package Service;
 
+import Entity.Department;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DepartmentService {
+    private static List<Department> departments = new ArrayList<>();
 
 
+    public static void addDepartment(Department department) {
+        if (department != null) {
+            departments.add(department);
+            System.out.println("department is added: " + department);
+        }
+        System.out.println("Cannot add null department ");
+    }
 }
