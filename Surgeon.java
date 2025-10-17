c7a1e4721021968d869727bd4aefb4bb7959c9ba
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-17 16:04:15 +0400

    Additional fields

--- Surgeon.java
+++ Surgeon.java
@@ -1,4 +1,13 @@
 package Entity;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Surgeon extends Doctor{
+
+    private static Integer surgeriesPerformed;
+    private List<String> surgeryTypes;
+    private Boolean operationTheatreAccess;
+
+    
 }
