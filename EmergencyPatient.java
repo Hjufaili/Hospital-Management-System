c20d95ac126fc07a82fdcdeb5ff619cbff716176
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-17 16:15:11 +0400

    Additional fields

--- EmergencyPatient.java
+++ EmergencyPatient.java
@@ -1,4 +1,8 @@
 package Entity;
 
 public class EmergencyPatient extends InPatient{
+    private String emergencyType;
+    private String arrivalMode;
+    private Integer triageLevel;
+    private Boolean admittedViaER;
 }
