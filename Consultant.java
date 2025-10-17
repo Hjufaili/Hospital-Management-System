a69ee335de2ce9af68f426bd921c4f352fd225bf
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-17 16:08:42 +0400

    Implement constructors

--- Consultant.java
+++ Consultant.java
@@ -8,4 +8,35 @@ public class Consultant extends Doctor{
     private Boolean onlineConsultationAvailable;
     private Integer consultationDuration;
 
+    public Consultant(List<String> consultationTypes, Boolean onlineConsultationAvailable,
+                      Integer consultationDuration) {
+        this.consultationTypes = consultationTypes;
+        this.onlineConsultationAvailable = onlineConsultationAvailable;
+        this.consultationDuration = consultationDuration;
+    }
+
+    public Consultant(String id, String firstName, String lastName, LocalDate dateOfBirth,
+                      String gender, String phoneNumber, String address, String email,
+                      String doctorId, String specialization, String qualification,
+                      Integer experienceYears, String departmentId, Double consultationFee,
+                      List<String> availableSlots, List<Patient> assignedPatients,
+                      List<String> consultationTypes, Boolean onlineConsultationAvailable,
+                      Integer consultationDuration) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, address, email, doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.consultationTypes = consultationTypes;
+        this.onlineConsultationAvailable = onlineConsultationAvailable;
+        this.consultationDuration = consultationDuration;
+    }
+
+    public Consultant(String doctorId, String specialization, String qualification,
+                      Integer experienceYears, String departmentId, Double consultationFee,
+                      List<String> availableSlots, List<Patient> assignedPatients,
+                      List<String> consultationTypes, Boolean onlineConsultationAvailable,
+                      Integer consultationDuration) {
+        super(doctorId, specialization, qualification, experienceYears, departmentId, consultationFee, availableSlots, assignedPatients);
+        this.consultationTypes = consultationTypes;
+        this.onlineConsultationAvailable = onlineConsultationAvailable;
+        this.consultationDuration = consultationDuration;
+    }
+
 }
