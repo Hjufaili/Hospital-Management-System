653cbe9c41fc8ccab1124187ad4d78e30ee39863
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-17 16:15:00 +0400

    Additional fields

--- OutPatient.java
+++ OutPatient.java
@@ -1,4 +1,9 @@
 package Entity;
 
+import java.time.LocalDate;
+
 public class OutPatient extends Patient{
+    private Integer visitCount;
+    private LocalDate lastVisitDate;
+    private String preferredDoctorId;
 }
