d16f854703ff038345ace13e47911c65068cfe49
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-17 16:14:49 +0400

    Additional fields

--- InPatient.java
+++ InPatient.java
@@ -1,4 +1,13 @@
 package Entity;
 
+import java.time.LocalDate;
+
 public class InPatient extends Patient{
+
+    private LocalDate admissionDate;
+    private LocalDate dischargeDate;
+    private String roomNumber;
+    private String bedNumber;
+    private String admittingDoctorId;
+    private Double dailyCharges;
 }
