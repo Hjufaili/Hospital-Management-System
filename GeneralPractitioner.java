c22d8279bcf023ab45f915b505dca5a3aeba7d3c
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-17 16:20:36 +0400

    Implement constructors

--- GeneralPractitioner.java
+++ GeneralPractitioner.java
@@ -1,8 +1,43 @@
 package Entity;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class GeneralPractitioner extends Doctor{
 
     private Boolean walkinAvailable;
     private Boolean homeVisitAvailable;
     private Boolean vaccinationCertified;
+
+    public GeneralPractitioner(Boolean walkinAvailable, Boolean homeVisitAvailable, Boolean vaccinationCertified) {
+        this.walkinAvailable = walkinAvailable;
+        this.homeVisitAvailable = homeVisitAvailable;
+        this.vaccinationCertified = vaccinationCertified;
+    }
+
+    public GeneralPractitioner(String id, String firstName, String lastName, LocalDate dateOfBirth,
+                               String gender, String phoneNumber, String address, String email,
+                               String doctorId, String specialization, String qualification,
+                               Integer experienceYears, String departmentId, Double consultationFee,
+                               List<String> availableSlots, List<Patient> assignedPatients,
+                               Boolean walkinAvailable, Boolean homeVisitAvailable,
+                               Boolean vaccinationCertified) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, address, email, doctorId,
+                specialization, qualification, experienceYears, departmentId, consultationFee,
+                availableSlots, assignedPatients);
+        this.walkinAvailable = walkinAvailable;
+        this.homeVisitAvailable = homeVisitAvailable;
+        this.vaccinationCertified = vaccinationCertified;
+    }
+
+    public GeneralPractitioner(String doctorId, String specialization, String qualification,
+                               Integer experienceYears, String departmentId, Double consultationFee,
+                               List<String> availableSlots, List<Patient> assignedPatients,
+                               Boolean walkinAvailable, Boolean homeVisitAvailable, Boolean vaccinationCertified) {
+        super(doctorId, specialization, qualification, experienceYears, departmentId, consultationFee,
+                availableSlots, assignedPatients);
+        this.walkinAvailable = walkinAvailable;
+        this.homeVisitAvailable = homeVisitAvailable;
+        this.vaccinationCertified = vaccinationCertified;
+    }
 }
