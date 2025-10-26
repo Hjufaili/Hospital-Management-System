a3006ef33e3fe82ba3d1068f22fdf0342834a66f
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-26 15:19:36 +0400

    Null Check Methods (Overloaded)

--- HelperUtils.java
+++ HelperUtils.java
@@ -1,5 +1,19 @@
 package Utils;
 
 public class HelperUtils {
-    
+    public static boolean isNull(Object obj) {
+        return obj == null;
+    }
+
+    public static boolean isNull(String str) {
+        return (str == null || str.trim().isEmpty());
+    }
+
+    public static boolean isNotNull(Object obj) {
+        return obj != null;
+    }
+
+    public static boolean isNotNull(String str) {
+        return (str != null && !str.trim().isEmpty());
+    }
 }
