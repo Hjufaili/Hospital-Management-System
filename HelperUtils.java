cc6072fe3f9b544b5a17b65700132c93185f3ce7
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-26 15:20:28 +0400

    String Validation Methods (Overloaded)

--- HelperUtils.java
+++ HelperUtils.java
@@ -1,5 +1,7 @@
 package Utils;
 
+import java.util.regex.Pattern;
+
 public class HelperUtils {
     public static boolean isNull(Object obj) {
         return obj == null;
@@ -15,5 +17,25 @@ public class HelperUtils {
 
     public static boolean isNotNull(String str) {
         return (str != null && !str.trim().isEmpty());
+    }
+
+
+    public static boolean isValidString(String str) {
+        return (str != null && !str.trim().isEmpty());
+    }
+
+    public static boolean isValidString(String str, int minLength) {
+        return (isValidString(str) && str.trim().length() >= minLength);
+    }
+
+    public static boolean isValidString(String str, int minLength, int maxLength) {
+        if (!isValidString(str)) return false;
+        int len = str.trim().length();
+        return (len >= minLength && len <= maxLength);
+    }
+
+    public static boolean isValidString(String str, String regex) {
+        if (!isValidString(str) || isNull(regex)) return false;
+        return Pattern.matches(regex, str.trim());
     }
 }
