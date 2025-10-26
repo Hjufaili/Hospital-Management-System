3e6f9612a1806e01cc39297b67936484e53dbaae
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-26 15:21:03 +0400

    ID Generation Methods (Overloaded)

--- HelperUtils.java
+++ HelperUtils.java
@@ -1,5 +1,7 @@
 package Utils;
 
+import java.util.Random;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 public class HelperUtils {
@@ -37,5 +39,32 @@ public class HelperUtils {
     public static boolean isValidString(String str, String regex) {
         if (!isValidString(str) || isNull(regex)) return false;
         return Pattern.matches(regex, str.trim());
+    }
+
+
+    public static String generateId() {
+        return UUID.randomUUID().toString();
+    }
+
+    public static String generateId(String prefix) {
+        int randomNum = new Random().nextInt(90000) + 10000; // 5 digits
+        return prefix + "-" + randomNum;
+    }
+
+    public static String generateId(String prefix, int length) {
+        if (length <= 0) length = 5;
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            sb.append(random.nextInt(10)); // add random digit 0–9
+        }
+
+        return prefix + "-" + sb.toString();
+    }
+
+    public static String generateId(String prefix, String suffix) {
+        int randomNum = new Random().nextInt(90000) + 10000;
+        return prefix + "-" + randomNum + "-" + suffix;
     }
 }
