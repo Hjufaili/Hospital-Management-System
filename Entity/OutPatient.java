42d8589c56c4a8d601e905083d237dad4cb897b6
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-11-12 08:28:35 +0400

    OutPatient Integration with Spring boot

--- Entity/OutPatient.java
+++ Entity/OutPatient.java
@@ -2,11 +2,21 @@ package Entity;
 
 import Interface.Displayable;
 import Utils.HelperUtils;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+@Setter
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@ToString(callSuper = true)
 public class OutPatient extends Patient implements Displayable {
     private Integer visitCount;
     private LocalDate lastVisitDate;
@@ -18,70 +28,44 @@ public class OutPatient extends Patient implements Displayable {
         this.preferredDoctorId = preferredDoctorId;
     }
 
-    public OutPatient(String id, String firstName, String lastName, LocalDate dateOfBirth,
-                      String gender, String phoneNumber, String address, String email,
-                      String patientId, String bloodGroup, List<String> allergies,
-                      String emergencyContact, LocalDate registrationDate, String insuranceId,
-                      List<MedicalRecord> medicalRecords, List<Appointment> appointments,
-                      Integer visitCount, LocalDate lastVisitDate, String preferredDoctorId) {
-        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, address, email,
-                patientId, bloodGroup, allergies, emergencyContact, registrationDate,
-                insuranceId, medicalRecords, appointments);
-        this.visitCount = visitCount;
-        this.lastVisitDate = lastVisitDate;
-        this.preferredDoctorId = preferredDoctorId;
-    }
+//    public OutPatient(String id, String firstName, String lastName, LocalDate dateOfBirth,
+//                      String gender, String phoneNumber, String address, String email,
+//                      String patientId, String bloodGroup, List<String> allergies,
+//                      String emergencyContact, LocalDate registrationDate, String insuranceId,
+//                      List<MedicalRecord> medicalRecords, List<Appointment> appointments,
+//                      Integer visitCount, LocalDate lastVisitDate, String preferredDoctorId) {
+//        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, address, email,
+//                patientId, bloodGroup, allergies, emergencyContact, registrationDate,
+//                insuranceId, medicalRecords, appointments);
+//        this.visitCount = visitCount;
+//        this.lastVisitDate = lastVisitDate;
+//        this.preferredDoctorId = preferredDoctorId;
+//    }
+//
+//    public OutPatient(String firstName, String lastName, String gender, String phoneNumber,
+//                      String bloodGroup, String preferredDoctorId) {
+//
+//
+//        super(
+//                HelperUtils.generateId("PER"), firstName, lastName, (LocalDate) null,
+//                gender, phoneNumber,
+//                "N/A",
+//                "N/A",
+//
+//                HelperUtils.generateId("PAT"), bloodGroup,
+//                new ArrayList<String>(),
+//                "N/A",
+//                LocalDate.now(),
+//                "N/A",
+//                new ArrayList<MedicalRecord>(),
+//                new ArrayList<Appointment>()
+//        );
+//
+//        this.visitCount = 1;
+//        this.lastVisitDate = LocalDate.now();
+//        this.preferredDoctorId = preferredDoctorId;
+//    }
 
-    public OutPatient(String firstName, String lastName, String gender, String phoneNumber,
-                      String bloodGroup, String preferredDoctorId) {
-
-
-        super(
-                HelperUtils.generateId("PER"), firstName, lastName, (LocalDate) null,
-                gender, phoneNumber,
-                "N/A",
-                "N/A",
-
-                HelperUtils.generateId("PAT"), bloodGroup,
-                new ArrayList<String>(),
-                "N/A",
-                LocalDate.now(),
-                "N/A",
-                new ArrayList<MedicalRecord>(),
-                new ArrayList<Appointment>()
-        );
-
-        this.visitCount = 1;
-        this.lastVisitDate = LocalDate.now();
-        this.preferredDoctorId = preferredDoctorId;
-    }
-
-
-
-
-    public Integer getVisitCount() {
-        return visitCount;
-    }
-
-    public void setVisitCount(Integer visitCount) {
-        this.visitCount = visitCount;
-    }
-
-    public LocalDate getLastVisitDate() {
-        return lastVisitDate;
-    }
-
-    public void setLastVisitDate(LocalDate lastVisitDate) {
-        this.lastVisitDate = lastVisitDate;
-    }
-
-    public String getPreferredDoctorId() {
-        return preferredDoctorId;
-    }
-
-    public void setPreferredDoctorId(String preferredDoctorId) {
-        this.preferredDoctorId = preferredDoctorId;
-    }
 
     public void scheduleFollowUp(LocalDate nextVisitDate) {
         System.out.println("Follow-up visit for " + getFirstName() +
