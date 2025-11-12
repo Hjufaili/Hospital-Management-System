4c7aafe80f67b2b6f25dca4fa0602c05196946c0
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-11-12 08:26:25 +0400

    Integration with Spring boot

--- Entity/InPatient.java
+++ Entity/InPatient.java
@@ -3,111 +3,73 @@ package Entity;
 import Interface.Billable;
 import Interface.Displayable;
 import Utils.HelperUtils;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
-public class InPatient extends Patient implements Displayable,Billable {
+@Setter
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class InPatient extends Patient implements Displayable, Billable {
 
-    private LocalDate admissionDate;
+    private LocalDate admissionDate = LocalDate.now();
     private LocalDate dischargeDate;
     private String roomNumber;
-    private String bedNumber;
+    private String bedNumber = "1";
     private String admittingDoctorId;
-    private Double dailyCharges;
+    private Double dailyCharges = 0.0;
 
-    public InPatient(String firstName, String lastName, String gender, String phoneNumber,
-                     String bloodGroup, String admittingDoctorId, String roomNumber, Double dailyCharges) {
-
-        super(
-                HelperUtils.generateId("PER"), firstName, lastName, (LocalDate) null,
-                gender, phoneNumber,
-                "N/A",
-                "N/A",
-
-
-                HelperUtils.generateId("PAT"), bloodGroup,
-                new ArrayList<String>(),
-                "N/A",
-                LocalDate.now(),
-                "N/A",
-                new ArrayList<MedicalRecord>(),
-                new ArrayList<Appointment>()
-        );
-
-        this.admissionDate = LocalDate.now();
-        this.dischargeDate = null;
-        this.roomNumber = roomNumber;
-        this.bedNumber = "1";
-        this.admittingDoctorId = admittingDoctorId;
-        this.dailyCharges = dailyCharges;
-    }
-
-    public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender,
-                     String phoneNumber, String address, String email, String patientId,
-                     String bloodGroup, List<String> allergies, String emergencyContact,
-                     LocalDate registrationDate, String insuranceId, List<MedicalRecord> medicalRecords,
-                     List<Appointment> appointments, LocalDate admissionDate, LocalDate dischargeDate,
-                     String roomNumber, String bedNumber, String admittingDoctorId, Double dailyCharges) {
-        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email,address,
-                patientId, bloodGroup, allergies, emergencyContact, registrationDate, insuranceId, medicalRecords, appointments);
-        this.admissionDate = admissionDate;
-        this.dischargeDate = dischargeDate;
-        this.roomNumber = roomNumber;
-        this.bedNumber = bedNumber;
-        this.admittingDoctorId = admittingDoctorId;
-        this.dailyCharges = dailyCharges;
-    }
-
-    public Double getDailyCharges() {
-        return dailyCharges;
-    }
-
-    public void setDailyCharges(Double dailyCharges) {
-        this.dailyCharges = dailyCharges;
-    }
-
-    public String getAdmittingDoctorId() {
-        return admittingDoctorId;
-    }
-
-    public void setAdmittingDoctorId(String admittingDoctorId) {
-        this.admittingDoctorId = admittingDoctorId;
-    }
-
-    public String getBedNumber() {
-        return bedNumber;
-    }
-
-    public void setBedNumber(String bedNumber) {
-        this.bedNumber = bedNumber;
-    }
-
-    public String getRoomNumber() {
-        return roomNumber;
-    }
-
-    public void setRoomNumber(String roomNumber) {
-        this.roomNumber = roomNumber;
-    }
-
-    public LocalDate getDischargeDate() {
-        return dischargeDate;
-    }
-
-    public void setDischargeDate(LocalDate dischargeDate) {
-        this.dischargeDate = dischargeDate;
-    }
-
-    public LocalDate getAdmissionDate() {
-        return admissionDate;
-    }
-
-    public void setAdmissionDate(LocalDate admissionDate) {
-        this.admissionDate = admissionDate;
-    }
+//    public InPatient(String firstName, String lastName, String gender, String phoneNumber,
+//                     String bloodGroup, String admittingDoctorId, String roomNumber, Double dailyCharges) {
+//
+//        super(
+//                HelperUtils.generateId("PER"), firstName, lastName, (LocalDate) null,
+//                gender, phoneNumber,
+//                "N/A",
+//                "N/A",
+//
+//
+//                HelperUtils.generateId("PAT"), bloodGroup,
+//                new ArrayList<String>(),
+//                "N/A",
+//                LocalDate.now(),
+//                "N/A",
+//                new ArrayList<MedicalRecord>(),
+//                new ArrayList<Appointment>()
+//        );
+//
+//        this.admissionDate = LocalDate.now();
+//        this.dischargeDate = null;
+//        this.roomNumber = roomNumber;
+//        this.bedNumber = "1";
+//        this.admittingDoctorId = admittingDoctorId;
+//        this.dailyCharges = dailyCharges;
+//    }
+//
+//    public InPatient(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender,
+//                     String phoneNumber, String address, String email, String patientId,
+//                     String bloodGroup, List<String> allergies, String emergencyContact,
+//                     LocalDate registrationDate, String insuranceId, List<MedicalRecord> medicalRecords,
+//                     List<Appointment> appointments, LocalDate admissionDate, LocalDate dischargeDate,
+//                     String roomNumber, String bedNumber, String admittingDoctorId, Double dailyCharges) {
+//        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address,
+//                patientId, bloodGroup, allergies, emergencyContact, registrationDate, insuranceId, medicalRecords, appointments);
+//        this.admissionDate = admissionDate;
+//        this.dischargeDate = dischargeDate;
+//        this.roomNumber = roomNumber;
+//        this.bedNumber = bedNumber;
+//        this.admittingDoctorId = admittingDoctorId;
+//        this.dailyCharges = dailyCharges;
+//    }
 
     public long calculateStayDuration() {
         if (dischargeDate == null) {
